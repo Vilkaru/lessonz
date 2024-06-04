f15a1c82257d9bccc7a3ec32e2564b884237ac9b
Vilkaru <otakyvvvv@gmail.com>
2024-06-04 23:05:59 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+
+homework = '12'
+time = '1.5'
+name_lesson = 'Python'
+busy_time_on_the_lesson = float(time)/int(homework)
+print('Курс:',name_lesson ,',','всего задач:',homework,',','затрачено часов:',time,',','среднее время выполнения',busy_time_on_the_lesson,'часа' )
