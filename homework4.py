ca9917d7f266e0eadb28fc4b6d9f3bb598ae003b
Vilkaru <otakyvvvv@gmail.com>
2024-06-06 13:08:02 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+
+my_string = input('Введите слово: ')
+print('Слово в верхнем регистре:',my_string.upper())
+print('Слово в нижнем регистре:',my_string.lower())
+print('Слово без пробелов:',my_string.replace(' ', ''))
+print('Первая буква слова:',my_string[0])
+print('Последняя буква слова:',my_string[-1])
