352ec4846a49fc1efc99c84f7c9e187c37f3b944
Vilkaru <otakyvvvv@gmail.com>
2024-07-08 09:42:41 +0300

    Add files via upload

---
+++ modul_3_3.py
@@ -0,0 +1,41 @@
+
+def print_params(a = 1, b = 'строка', c = True):
+     print(a, b, c)
+# 1 point
+#print_params() - код исправно работает
+#print_params(a=4,b=15,c=22,d=4) - если аргументa нет в функции, то код будет выдавать ошибку
+#print_params(b=25) - код исправно работает
+#print_params(c = [1,2,3]) - код исправно работает
+# 2 point
+#values_list =[12, False, 'Lol']
+#values_dict ={"a": 23, "b": True, 'c': "urban"}
+#print_params(**values_dict)
+#print_params(*values_list)
+# 3 point
+values_list_2 = [False, 52.12]
+print_params(*values_list_2, 42)
+
+
+
+
+
+            
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
