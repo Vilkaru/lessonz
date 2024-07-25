728a42215092cff4f68d6126bd34116ddd8b8d15
Vilkaru <otakyvvvv@gmail.com>
2024-07-25 21:46:27 +0300

    Add files via upload

---
+++ Fake_math.py
@@ -0,0 +1,9 @@
+
+def divide(first, second):
+   c = float()
+   if second != 0:
+       c = first / second
+       return c
+   else:
+       return 'Ошибка'
+
---
+++ modul_4_1.py
@@ -0,0 +1,37 @@
+
+import Fake_math as Fm
+import true_math as tm
+
+result1 = Fm.divide(69, 3)
+result2 = Fm.divide(3, 0)
+result3 = tm.divide(49, 7)
+result4 = tm.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+
+
+
+
+            
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
